4701c5a9901e2c01039d13f97f5aad09ef4f35ef
aisy <aisymuhammad@gmail.com>
2020-06-29 21:43:16 +0700

    add sample firebase get data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-row>
+        <v-col cols="3" v-for="book in books" :key="book.id">
+          <v-card>
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title>{{book.judul_buku}}</v-card-title>
+            </v-img>
+            <v-card-text>{{book.penerbit}}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import firebase from "../config/firebase";
+const db = firebase.firestore();
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      books: []
+    };
+  },
+  methods: {
+    getData() {
+      db.collection("books").onSnapshot(snapshot => {
+        const listItem = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        this.books = listItem;
+      });
+    }
+  },
+  mounted() {
+    this.getData();
   }
-}
+};
 </script>
