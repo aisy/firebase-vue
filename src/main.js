3aa8f54f42407e713a26cb9b8f3990b1e9795228
aisy <aisymuhammad@gmail.com>
2020-06-29 21:40:25 +0700

    configure project

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from './config/firebase'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  firebase,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
